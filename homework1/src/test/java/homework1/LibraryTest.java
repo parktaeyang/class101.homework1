65a396fad2ba2497a9ced656ed833441e17ddc75
pty10 <pty10@DESKTOP-TH11PNB>
2020-09-25 21:54:25 +0900

    class101 h.w  Junit 테스트 추가   - 상품목록생성 테스트   - 주문 시 입력 테스트   - 주문상품가격계산 테스트

--- homework1/src/test/java/homework1/LibraryTest.java
+++ homework1/src/test/java/homework1/LibraryTest.java
@@ -3,12 +3,6 @@
  */
 package homework1;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
+   
 }
